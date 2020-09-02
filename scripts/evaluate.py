24cf50741792d789468e42b710c0c5e925a1cf76
Chris Wallace <cjwallace@cloudera.com>
2020-09-02 17:48:22 +0000

    Add new, seasonally-complex Prophet model
    Added a model with four separate daily seasonalities for between summer/winter weekdays/weekends. On MAPE, it performs worse than both baseline and default prophet. To improve any STS model, I need better diagnostics.

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -1,7 +1,11 @@
 import numpy as np
 
 from sts.models.baselines import year_ahead_hourly_forecast
-from sts.models.prophet import default_prophet_model
+from sts.models.prophet import (
+    default_prophet_model,
+    add_season_weekday_indicators,
+    seasonal_daily_prophet_model
+)
 from sts.data.loader import load_california_electricity_demand
 
 
@@ -55,10 +59,39 @@ prophet_results['ape'] = np.abs(
 prophet_mape = prophet_results.ape.mean()
 
 
+# ## Prophet (with more complicated seasonality)
+# FB Prophet model, splitting intra-day seasonalities into four subgroups:
+# - summer weekday
+# - summer weekend
+# - winter weekday
+# - winter weekend
+
+
+seasonal_model = seasonal_daily_prophet_model(train_df)
+
+seasonal_future = add_season_weekday_indicators(future)
+seasonal_forecast = seasonal_model.predict(seasonal_future)
+
+seasonal_prophet_results = (
+    df
+    [(df['ds'] >= '2019') & (df['ds'] < '2020')]
+    .merge(seasonal_forecast, on='ds')
+)
+
+seasonal_prophet_results['ape'] = np.abs(
+    (seasonal_prophet_results.y - seasonal_prophet_results.yhat) / seasonal_prophet_results.y
+)
+
+seasonal_prophet_mape = seasonal_prophet_results.ape.mean()
+
+
+
+
 # ## Compare
 
 print(f'Baseline MAPE over 2019: {baseline_mape:.3f}')
 print(f'Prophet MAPE over 2019: {prophet_mape:.3f}')
+print(f'Seasonal Prophet MAPE over 2019: {seasonal_prophet_mape:.3f}')
 
 # There are problems with MAPE.
 # Should investigate MASE (https://robjhyndman.com/papers/mase.pdf)
--- sts/models/prophet.py
+++ sts/models/prophet.py
@@ -1,11 +1,87 @@
 from fbprophet import Prophet
 
+
 def default_prophet_model(df):
     model = Prophet()
     model.fit(df)
     return model
 
+
 def multiplicative_prophet_model(df):
     model = Prophet(seasonality_mode='multiplicative')
     model.fit(df)
     return model
+
+
+def seasonal_daily_prophet_model(df):
+    model = Prophet(
+        daily_seasonality=False,
+        yearly_seasonality=5
+    )
+    model.add_seasonality(
+        name='winter_weekday',
+        period=24,
+        fourier_order=4,
+        condition_name='winter_weekday'
+    )
+    model.add_seasonality(
+        name='winter_weekend',
+        period=24,
+        fourier_order=4,
+        condition_name='winter_weekend'
+    )
+    model.add_seasonality(
+        name='summer_weekday',
+        period=24,
+        fourier_order=4,
+        condition_name='summer_weekday'
+    )
+    model.add_seasonality(
+        name='summer_weekend',
+        period=24,
+        fourier_order=4,
+        condition_name='summer_weekend'
+    )
+    df = add_season_weekday_indicators(df)
+    model.fit(df)
+    return model
+
+
+def add_season_weekday_indicators(df):
+    df['winter_weekday'] = df['ds'].apply(is_winter_weekday)
+    df['winter_weekend'] = df['ds'].apply(is_winter_weekend)
+    df['summer_weekday'] = df['ds'].apply(is_summer_weekday)
+    df['summer_weekend'] = df['ds'].apply(is_summer_weekend)
+    return df
+
+
+def is_winter_weekday(ds):
+    condition = (
+        (ds.month < 6 or ds.month >= 10)
+        and not (ds.day_name() in ['Saturday', 'Sunday'])
+    )
+    return condition
+
+
+def is_winter_weekend(ds):
+    condition = (
+        (ds.month < 6 or ds.month >= 10)
+        and (ds.day_name() in ['Saturday', 'Sunday'])
+    )
+    return condition
+
+
+def is_summer_weekday(ds):
+    condition = (
+        (ds.month >= 6 or ds.month < 10)
+        and not (ds.day_name() in ['Saturday', 'Sunday'])
+    )
+    return condition
+
+
+def is_summer_weekend(ds):
+    condition = (
+        (ds.month >= 6 or ds.month < 10)
+        and (ds.day_name() in ['Saturday', 'Sunday'])
+    )
+    return condition
