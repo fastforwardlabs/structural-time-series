7179e1216482b99caef202eaa859ff58497b7268
Chris Wallace <cjwallace@cloudera.com>
2020-09-03 17:20:44 +0000

    Create setup.py to make sts module pip installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sts',
+    version='0.0.1',
+    description='''
+        Utilities for structural time series modelling of
+        California electricity demand data.
+    ''',
+    author='Chris J. Wallace',
+    packages=['sts']
+)
