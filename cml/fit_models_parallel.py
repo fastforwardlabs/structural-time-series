bc847518b397224cd1bf8c4459b8bfdd0ea2160d
andrewrreed <areed1242@gmail.com>
2020-11-12 17:16:05 -0500

    Create script to launch model training in paralllel

---
+++ cml/fit_models_parallel.py
@@ -0,0 +1,23 @@
+import os
+import time
+import cdsw
+
+def fit_models_parallel():
+    '''
+    Use the CDSW Workers API (via Python SDK) to launch each model fitting script in parallel
+
+    Docs - https://docs.cloudera.com/machine-learning/cloud/distributed-computing/topics/ml-workers-api.html
+
+    '''
+    # Launch a separate worker to run each script independently
+    
+    base_path = os.getcwd()
+    script_path = base_path + '/scripts'
+
+    scripts = os.listdir(script_path)
+    scripts = [script_path+'/'+script for script in scripts if script[0:3] in ['fit','mak']
+
+    for script in scripts:
+        cdsw.launch_workers(n=1, cpu=1, memory=2, script=script)
+if __name__ == "__main__":
+    fit_models_parallel()
