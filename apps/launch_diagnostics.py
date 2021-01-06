8a2058b92e3ad868e5dcd12d8234f1cec48edcaa
andrewrreed <areed1242@gmail.com>
2021-01-06 11:57:34 -0500

    Hotfix for scripts to launch apps
    It appears the code auto-formatter used in the AMP Review introduced a syntax error for these files.

--- apps/launch_diagnostics.py
+++ apps/launch_diagnostics.py
@@ -38,4 +38,4 @@
 #
 # ###########################################################################
 
-!streamlit run apps/diagnostics.py - -server.port $CDSW_APP_PORT - -server.address 127.0.0.1
+!streamlit run apps/diagnostics.py --server.port $CDSW_APP_PORT --server.address 127.0.0.1
--- apps/launch_forecast.py
+++ apps/launch_forecast.py
@@ -38,4 +38,4 @@
 #
 # ###########################################################################
 
-!streamlit run apps/forecast.py - -server.port $CDSW_APP_PORT - -server.address 127.0.0.1
+!streamlit run apps/forecast.py --server.port $CDSW_APP_PORT --server.address 127.0.0.1
